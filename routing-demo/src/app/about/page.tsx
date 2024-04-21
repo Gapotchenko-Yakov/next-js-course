01f9edb1af79f7910a31281cd29ea9f4cbf702af
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-21 19:34:03 +0300

    Routing Metadata demo

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+export const metadata = {
+  title: "About My Project",
+};
+
 const About = () => {
   return <h1>About</h1>;
 };
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,6 +1,21 @@
+import { Metadata } from "next";
 import React from "react";
 
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve, reject) =>
+    setTimeout(() => resolve(`iPhone ${params.productId}`), 100)
+  );
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+type Props = { params: { productId: string } };
+
+const ProductDetails = ({ params }: Props) => {
   return <h2>Details about product {params.productId}</h2>;
 };
 
