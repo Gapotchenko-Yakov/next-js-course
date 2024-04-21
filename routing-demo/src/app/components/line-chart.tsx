5c541554baba2e94b01e319e548727ff665f4469
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-21 11:54:20 +0300

    File Colocation

---
+++ routing-demo/src/app/components/line-chart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LineChart = () => {
+  return <h2>Line Chart</h2>;
+};
+
+export default LineChart;
---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import LineChart from "./line-chart";
+
+const BarChart = () => {
+  return <h2>Bar Chart</h2>;
+};
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <LineChart />
+      <BarChart />
+    </div>
+  );
+};
+
+export default Dashboard;
