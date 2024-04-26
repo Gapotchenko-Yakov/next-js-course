cccb091a9067de4089401a4476dbd2f9b6709ffe
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-26 15:53:40 +0300

    Intercepting Routes

---
+++ routing-demo/src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "About My Project",
+};
+
+const InterceptedAbout = () => {
+  return <h1>(...) Intercepted About</h1>;
+};
+
+export default InterceptedAbout;
