e515317a4e0e7824a4d626c2756a3419e5219824
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-27 13:56:18 +0300

    Headers in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export const GET = async () => {
-  return new Response("Profile API data");
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export const GET = async (request: NextRequest) => {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 };
