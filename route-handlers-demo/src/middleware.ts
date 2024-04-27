2821b113529a8a3542f4af6225ab0b7e90d83f4d
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-27 21:51:26 +0300

    Middleware cookies and headers

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -2,9 +2,19 @@ import { match } from "assert";
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.rewrite(new URL("/hello", request.url));
+  const response = NextResponse.next();
+
+  if (!request.cookies.get("theme")) {
+    response.cookies.set("theme", "dark");
   }
+
+  response.headers.set("custom-header", "custom-value");
+
+  return response;
+
+  //   if (request.nextUrl.pathname === "/profile") {
+  //     return NextResponse.rewrite(new URL("/hello", request.url));
+  //   }
 
   //   return NextResponse.redirect(new URL("/", request.url));
 }
