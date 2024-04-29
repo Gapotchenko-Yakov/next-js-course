64550a7058de63cfc36ffd7fe07e179fd6cb77e9
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-29 19:54:11 +0300

    Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
+import { cookies } from "next/headers";
+
 const AboutPage = () => {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log("🚀 ~ AboutPage ~ theme:", theme);
   console.log("About server component");
   return <div>About Page {new Date().toLocaleTimeString()}</div>;
 };
